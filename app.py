f1129804cecbac0cfb8a66cf36051d50bfc1847e
bbigmic <122184799+bbigmic@users.noreply.github.com>
2024-11-05 20:56:41 +0100

    zmiana DATABASE_URI na DATABASE_URL, usuniecie database.db z .gitignore

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pytz
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///database.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
