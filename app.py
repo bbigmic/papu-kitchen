4baa59eadb3e65e5802133b79d838c2c792a74ca
bbigmic <122184799+bbigmic@users.noreply.github.com>
2024-11-05 22:27:38 +0100

    dodanie debug false, dodanie gunicorn do requirements

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///dat
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
+app.config['DEBUG'] = False
 app.config['UPLOAD_FOLDER'] = 'static/images'  # Ścieżka do przechowywania zdjęć
 
 # Modele bazy danych
